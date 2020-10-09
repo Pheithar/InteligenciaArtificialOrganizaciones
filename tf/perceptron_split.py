f973fbad31226859d4c18ed9e4696ecd0d5f1303
pheithar <pheithar@hotmail.com>
2020-10-09 13:01:17 +0200

    split done

--- tf/perceptron_split.py
+++ tf/perceptron_split.py
@@ -28,17 +28,20 @@ csvData_last30 = np.array(csvData[csvData.columns[-31:-1]])
 
 csvData_last60 = np.array(csvData[csvData.columns[-61:-1]])
 
+size_train = int(len(csvData_lastDay) * 0.7)
+size_test = int(len(csvData_lastDay) - size_train)
 
-list_error_7_days = []
+csvData_training_Y = csvData_lastDay[:size_train]
+csvData_test_Y = csvData_lastDay[size_train:]
 
-list_error_15_days = []
-
-list_error_30_days = []
-
-list_error_60_days = []
-
+print()
+print()
+print()
 
 # Modelo 7 días
+print("Generando modelo con 7 días...")
+csvData_last7_training_X = csvData_last7[:size_train]
+csvData_last7_test_X = csvData_last7[size_train:]
 
 model_7_days = Sequential()
 model_7_days.add(Dense(64, input_shape=INPUT_SHAPE_7, activation='relu'))
@@ -49,17 +52,22 @@ model_7_days.compile(loss='mean_absolute_error',
                     optimizer='adam',
                     metrics=['mean_absolute_error'])
 
-model_7_days.fit(csvData_last7, csvData_lastDay,
+model_7_days.fit(csvData_last7_training_X, csvData_training_Y,
                   batch_size=BATCH_SIZE,
                   epochs=EPOCHS,
+                  validation_data=(csvData_last7_test_X, csvData_test_Y),
                   verbose=0)
 
-model_7_days.save('../output/model_7_days')
+_, test_err_7_days = model_7_days.evaluate(csvData_last7_test_X, csvData_test_Y, verbose=0)
 
+model_7_days.save('../output/model_7_days_split')
 
 
 
 # Modelo 15 días
+print("Generando modelo con 15 días...")
+csvData_last15_training_X = csvData_last15[:size_train]
+csvData_last15_test_X = csvData_last15[size_train:]
 
 model_15_days = Sequential()
 model_15_days.add(Dense(64, input_shape=INPUT_SHAPE_15, activation='relu'))
@@ -70,16 +78,22 @@ model_15_days.compile(loss='mean_absolute_error',
                     optimizer='adam',
                     metrics=['mean_absolute_error'])
 
-model_15_days.fit(csvData_last15, csvData_lastDay,
+model_15_days.fit(csvData_last15_training_X, csvData_training_Y,
                   batch_size=BATCH_SIZE,
                   epochs=EPOCHS,
+                  validation_data=(csvData_last15_test_X, csvData_test_Y),
                   verbose=0)
 
-model_15_days.save('../output/model_15_days')
+_, test_err_15_days = model_15_days.evaluate(csvData_last15_test_X, csvData_test_Y, verbose=0)
+
+model_15_days.save('../output/model_15_days_split')
 
 
 
 # Modelo 30 días
+print("Generando modelo con 30 días...")
+csvData_last30_training_X = csvData_last30[:size_train]
+csvData_last30_test_X = csvData_last30[size_train:]
 
 model_30_days = Sequential()
 model_30_days.add(Dense(128, input_shape=INPUT_SHAPE_30, activation='relu'))
@@ -90,18 +104,22 @@ model_30_days.compile(loss='mean_absolute_error',
                     optimizer='adam',
                     metrics=['mean_absolute_error'])
 
-model_30_days.fit(csvData_last30, csvData_lastDay,
+model_30_days.fit(csvData_last30_training_X, csvData_training_Y,
                   batch_size=BATCH_SIZE,
                   epochs=EPOCHS,
+                  validation_data=(csvData_last30_test_X, csvData_test_Y),
                   verbose=0)
 
-model_30_days.save('../output/model_30_days')
+_, test_err_30_days = model_30_days.evaluate(csvData_last30_test_X, csvData_test_Y, verbose=0)
 
-
+model_30_days.save('../output/model_30_days_split')
 
 
 
 # Modelo 60 días
+print("Generando modelo con 60 días...")
+csvData_last60_training_X = csvData_last60[:size_train]
+csvData_last60_test_X = csvData_last60[size_train:]
 
 model_60_days = Sequential()
 model_60_days.add(Dense(128, input_shape=INPUT_SHAPE_60, activation='relu'))
@@ -112,21 +130,24 @@ model_60_days.compile(loss='mean_absolute_error',
                     optimizer='adam',
                     metrics=['mean_absolute_error'])
 
-model_60_days.fit(csvData_last60, csvData_lastDay,
+model_60_days.fit(csvData_last60_training_X, csvData_training_Y,
                   batch_size=BATCH_SIZE,
                   epochs=EPOCHS,
+                  validation_data=(csvData_last60_test_X, csvData_test_Y),
                   verbose=0)
 
-model_60_days.save('../output/model_60_days')
+_, test_err_60_days = model_60_days.evaluate(csvData_last60_test_X, csvData_test_Y, verbose=0)
+
+model_60_days.save('../output/model_60_days_split')
 
 
 
 
 
-error_7_string = "Error total de la red de 7 días es: " + str(sum(list_error_7_days)/len(list_error_7_days))
-error_15_string = "Error total de la red de 15 días es: " + str(sum(list_error_15_days)/len(list_error_15_days))
-error_30_string = "Error total de la red de 30 días es: " + str(sum(list_error_30_days)/len(list_error_30_days))
-error_60_string = "Error total de la red de 60 días es: " + str(sum(list_error_60_days)/len(list_error_60_days))
+error_7_string = "Error de la red de 7 días es: " + str(test_err_7_days)
+error_15_string = "Error de la red de 15 días es: " + str(test_err_15_days)
+error_30_string = "Error de la red de 30 días es: " + str(test_err_30_days)
+error_60_string = "Error de la red de 60 días es: " + str(test_err_60_days)
 
 
 
@@ -139,7 +160,7 @@ print(error_30_string)
 print()
 print(error_60_string)
 
-with open('../output/out.txt', 'w') as file:
+with open('../output/out_split.txt', 'w') as file:
     file.write(error_7_string + '\n')
     file.write(error_15_string + '\n')
     file.write(error_30_string + '\n')
--- tf/predict.py
+++ tf/predict.py
@@ -12,6 +12,11 @@ model_15_days = tf.keras.models.load_model('../output/model_15_days')
 model_30_days = tf.keras.models.load_model('../output/model_30_days')
 model_60_days = tf.keras.models.load_model('../output/model_60_days')
 
+model_7_days_split = tf.keras.models.load_model('../output/model_7_days_split')
+model_15_days_split = tf.keras.models.load_model('../output/model_15_days_split')
+model_30_days_split = tf.keras.models.load_model('../output/model_30_days_split')
+model_60_days_split = tf.keras.models.load_model('../output/model_60_days_split')
+
 
 
 spain = None
@@ -29,13 +34,14 @@ spain_15_days =  np.reshape(spain[-15:].astype(np.float32), (1, 15))
 spain_30_days =  np.reshape(spain[-30:].astype(np.float32), (1, 30))
 spain_60_days =  np.reshape(spain[-60:].astype(np.float32), (1, 60))
 
-
 brazil_7_days = np.reshape(brazil[-7:].astype(np.float32), (1, 7))
 brazil_15_days =  np.reshape(brazil[-15:].astype(np.float32), (1, 15))
 brazil_30_days =  np.reshape(brazil[-30:].astype(np.float32), (1, 30))
 brazil_60_days =  np.reshape(brazil[-60:].astype(np.float32), (1, 60))
 
 title = "Valores reales"
+title_kfold = "CROSSVALIDATION"
+title_split = "SPLIT PERCENTAGE"
 title_7 = "Predicción del modelo de 7 días"
 title_15 = "Predicción del modelo de 15 días"
 title_30 = "Predicción del modelo de 30 días"
@@ -66,24 +72,31 @@ def predictions_3(model, data):
 
 # Predicciones datos 7 días
 (spain_7_pred1, spain_7_pred2, spain_7_pred3) = predictions_3(model_7_days, spain_7_days)
-
 (brazil_7_pred1, brazil_7_pred2, brazil_7_pred3) = predictions_3(model_7_days, brazil_7_days)
+
+(spain_7_pred1_split, spain_7_pred2_split, spain_7_pred3_split) = predictions_3(model_7_days_split, spain_7_days)
+(brazil_7_pred1_split, brazil_7_pred2_split, brazil_7_pred3_split) = predictions_3(model_7_days_split, brazil_7_days)
 
 # Predicciones datos 15 días
 (spain_15_pred1, spain_15_pred2, spain_15_pred3) = predictions_3(model_15_days, spain_15_days)
-
 (brazil_15_pred1, brazil_15_pred2, brazil_15_pred3) = predictions_3(model_15_days, brazil_15_days)
+
+(spain_15_pred1_split, spain_15_pred2_split, spain_15_pred3_split) = predictions_3(model_15_days_split, spain_15_days)
+(brazil_15_pred1_split, brazil_15_pred2_split, brazil_15_pred3_split) = predictions_3(model_15_days_split, brazil_15_days)
 
 # Predicciones datos 30 días
 (spain_30_pred1, spain_30_pred2, spain_30_pred3) = predictions_3(model_30_days, spain_30_days)
-
 (brazil_30_pred1, brazil_30_pred2, brazil_30_pred3) = predictions_3(model_30_days, brazil_30_days)
+
+(spain_30_pred1_split, spain_30_pred2_split, spain_30_pred3_split) = predictions_3(model_30_days_split, spain_30_days)
+(brazil_30_pred1_split, brazil_30_pred2_split, brazil_30_pred3_split) = predictions_3(model_30_days_split, brazil_30_days)
 
 # Predicciones datos 60 días
 (spain_60_pred1, spain_60_pred2, spain_60_pred3) = predictions_3(model_60_days, spain_60_days)
-
 (brazil_60_pred1, brazil_60_pred2, brazil_60_pred3) = predictions_3(model_60_days, brazil_60_days)
 
+(spain_60_pred1_split, spain_60_pred2_split, spain_60_pred3_split) = predictions_3(model_60_days_split, spain_60_days)
+(brazil_60_pred1_split, brazil_60_pred2_split, brazil_60_pred3_split) = predictions_3(model_60_days_split, brazil_60_days)
 
 
 # Prints
@@ -93,54 +106,119 @@ print(title)
 print("España día 1:", real_values_spain[0])
 print("España día 2:", real_values_spain[1])
 print("España día 3:", real_values_spain[2])
-
+print()
 print("Brasil día 1:", real_values_brazil[0])
 print("Brasil día 2:", real_values_brazil[1])
 print("Brasil día 3:", real_values_brazil[2])
 
+print()
+print()
+
+print(title_kfold)
+
+print()
 print()
 print(title_7)
 
 print("España predicción 1:", spain_7_pred1, "--- Error:", spain_7_pred1 - real_values_spain[0])
 print("España predicción 2:", spain_7_pred2, "--- Error:", spain_7_pred2 - real_values_spain[1])
 print("España predicción 3:", spain_7_pred3, "--- Error:", spain_7_pred3 - real_values_spain[2])
-
+print()
 print("Brasil predicción 1:", brazil_7_pred1, "--- Error:", brazil_7_pred1 - real_values_brazil[0])
 print("Brasil predicción 2:", brazil_7_pred2, "--- Error:", brazil_7_pred2 - real_values_brazil[1])
 print("Brasil predicción 3:", brazil_7_pred3, "--- Error:", brazil_7_pred3 - real_values_brazil[2])
 
 
 print()
+print()
 print(title_15)
 
 print("España predicción 1:", spain_15_pred1, "--- Error:", spain_15_pred1 - real_values_spain[0])
 print("España predicción 2:", spain_15_pred2, "--- Error:", spain_15_pred2 - real_values_spain[1])
 print("España predicción 3:", spain_15_pred3, "--- Error:", spain_15_pred3 - real_values_spain[2])
-
+print()
 print("Brasil predicción 1:", brazil_15_pred1, "--- Error:", brazil_15_pred1 - real_values_brazil[0])
 print("Brasil predicción 2:", brazil_15_pred2, "--- Error:", brazil_15_pred2 - real_values_brazil[1])
 print("Brasil predicción 3:", brazil_15_pred3, "--- Error:", brazil_15_pred3 - real_values_brazil[2])
 
 
 print()
+print()
 print(title_30)
 
 print("España predicción 1:", spain_30_pred1, "--- Error:", spain_30_pred1 - real_values_spain[0])
 print("España predicción 2:", spain_30_pred2, "--- Error:", spain_30_pred2 - real_values_spain[1])
 print("España predicción 3:", spain_30_pred3, "--- Error:", spain_30_pred3 - real_values_spain[2])
-
+print()
 print("Brasil predicción 1:", brazil_30_pred1, "--- Error:", brazil_30_pred1 - real_values_brazil[0])
 print("Brasil predicción 2:", brazil_30_pred2, "--- Error:", brazil_30_pred2 - real_values_brazil[1])
 print("Brasil predicción 3:", brazil_30_pred3, "--- Error:", brazil_30_pred3 - real_values_brazil[2])
 
 
 print()
+print()
 print(title_60)
 
 print("España predicción 1:", spain_60_pred1, "--- Error:", spain_60_pred1 - real_values_spain[0])
 print("España predicción 2:", spain_60_pred2, "--- Error:", spain_60_pred2 - real_values_spain[1])
 print("España predicción 3:", spain_60_pred3, "--- Error:", spain_60_pred3 - real_values_spain[2])
-
+print()
 print("Brasil predicción 1:", brazil_60_pred1, "--- Error:", brazil_60_pred1 - real_values_brazil[0])
 print("Brasil predicción 2:", brazil_60_pred2, "--- Error:", brazil_60_pred2 - real_values_brazil[1])
 print("Brasil predicción 3:", brazil_60_pred3, "--- Error:", brazil_60_pred3 - real_values_brazil[2])
+
+print()
+print()
+
+print(title_split)
+
+print()
+print()
+print(title_7)
+
+print("España predicción 1:", spain_7_pred1_split, "--- Error:", spain_7_pred1_split - real_values_spain[0])
+print("España predicción 2:", spain_7_pred2_split, "--- Error:", spain_7_pred2_split - real_values_spain[1])
+print("España predicción 3:", spain_7_pred3_split, "--- Error:", spain_7_pred3_split - real_values_spain[2])
+print()
+print("Brasil predicción 1:", brazil_7_pred1_split, "--- Error:", brazil_7_pred1_split - real_values_brazil[0])
+print("Brasil predicción 2:", brazil_7_pred2_split, "--- Error:", brazil_7_pred2_split - real_values_brazil[1])
+print("Brasil predicción 3:", brazil_7_pred3_split, "--- Error:", brazil_7_pred3_split - real_values_brazil[2])
+
+
+print()
+print()
+print(title_15)
+
+print("España predicción 1:", spain_15_pred1_split, "--- Error:", spain_15_pred1_split - real_values_spain[0])
+print("España predicción 2:", spain_15_pred2_split, "--- Error:", spain_15_pred2_split - real_values_spain[1])
+print("España predicción 3:", spain_15_pred3_split, "--- Error:", spain_15_pred3_split - real_values_spain[2])
+print()
+print("Brasil predicción 1:", brazil_15_pred1_split, "--- Error:", brazil_15_pred1_split - real_values_brazil[0])
+print("Brasil predicción 2:", brazil_15_pred2_split, "--- Error:", brazil_15_pred2_split - real_values_brazil[1])
+print("Brasil predicción 3:", brazil_15_pred3_split, "--- Error:", brazil_15_pred3_split - real_values_brazil[2])
+
+
+print()
+print()
+print(title_30)
+
+print("España predicción 1:", spain_30_pred1_split, "--- Error:", spain_30_pred1_split - real_values_spain[0])
+print("España predicción 2:", spain_30_pred2_split, "--- Error:", spain_30_pred2_split - real_values_spain[1])
+print("España predicción 3:", spain_30_pred3_split, "--- Error:", spain_30_pred3_split - real_values_spain[2])
+print()
+print("Brasil predicción 1:", brazil_30_pred1_split, "--- Error:", brazil_30_pred1_split - real_values_brazil[0])
+print("Brasil predicción 2:", brazil_30_pred2_split, "--- Error:", brazil_30_pred2_split - real_values_brazil[1])
+print("Brasil predicción 3:", brazil_30_pred3_split, "--- Error:", brazil_30_pred3_split - real_values_brazil[2])
+
+
+print()
+print()
+print(title_60)
+
+print("España predicción 1:", spain_60_pred1_split, "--- Error:", spain_60_pred1_split - real_values_spain[0])
+print("España predicción 2:", spain_60_pred2_split, "--- Error:", spain_60_pred2_split - real_values_spain[1])
+print("España predicción 3:", spain_60_pred3_split, "--- Error:", spain_60_pred3_split - real_values_spain[2])
+print()
+print("Brasil predicción 1:", brazil_60_pred1_split, "--- Error:", brazil_60_pred1_split - real_values_brazil[0])
+print("Brasil predicción 2:", brazil_60_pred2_split, "--- Error:", brazil_60_pred2_split - real_values_brazil[1])
+print("Brasil predicción 3:", brazil_60_pred3_split, "--- Error:", brazil_60_pred3_split - real_values_brazil[2])
